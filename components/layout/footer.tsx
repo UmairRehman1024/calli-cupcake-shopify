65c008e572c61944ac5d9bb8194c2b286ef43e96
UmairRehman1024 <85501421+UmairRehman1024@users.noreply.github.com>
2025-04-22 13:39:28 +0100

    chnaged logo image size on footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
 import FooterMenu from 'components/layout/footer-menu';
-import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
 import { Suspense } from 'react';
+import LogoIcon from '../icons/logo';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
 
@@ -19,7 +19,7 @@ export default async function Footer() {
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
         <div>
           <Link className="flex items-center gap-2 text-black dark:text-white md:pt-1" href="/">
-            <LogoSquare size="sm" />
+            <LogoIcon className="h-16 w-16" />
             <span className="uppercase">{SITE_NAME}</span>
           </Link>
         </div>
